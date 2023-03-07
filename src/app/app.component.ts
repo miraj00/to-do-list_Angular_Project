227f1435d02b13197f91ed28842e86063b2ceb19
miraj <miraj00@gmail.com>
2023-03-06 22:40:34 -0500

    input box and buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,10 @@ import { Todo } from 'todo';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
+
 
 
 export class AppComponent {
