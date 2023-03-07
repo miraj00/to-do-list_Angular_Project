509115dc69e5580aaab6fe73422c0d02d6728973
miraj <miraj00@gmail.com>
2023-03-06 20:58:11 -0500

    form module added, functions added, html edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { Todo } from 'todo';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'To-Do-List';
+ 
+      title = 'To-Do-List';
 
+      todoList: Todo[] = [];
 
-       todo :Todo [] = [
+      completedTodos: Todo[] = [];
+
+      filteredIndexes: number[] = [];
+      filteredList: Todo[] = [];
+
+  // Pre inserted array 
+      todo :Todo [] = [
           {
             task: 'Do Dishes',
             completed: false,
@@ -26,6 +38,60 @@ export class AppComponent {
             task: 'Do Vacuum',
             completed: false,
           },
-        ];
+    ];
 
+
+    // ------------------------  Functions to add, remove, complete and search ---------------------------------------
+    // addTodo function
+    addTodo(todo: Todo): void {
+      this.todoList = [...this.todoList, todo];
+    }
+  
+
+    // removeTodo function will map the index and then splice value using that index :
+    removeTodo(todoIn: string): void {
+          console.log('removeTodo: ' + todoIn);
+          let index = 0;
+  
+      this.todoList.map((todo, i) => {
+        if (todo.task === todoIn) {
+          index = i;
+        }
+        return index;
+      });
+      this.todoList.splice(index, 1);
+    }
+  
+    //completeTodo function will give true value to completed tasks and add in tpo array :
+    completeTodo(todo: string, i: number): void {
+          let tempTodo = {
+            task: todo,
+            completed: true,
+          };
+  
+         console.log(this.todoList[i]);
+  
+      this.todoList[i].completed = !this.todoList[i].completed;
+  
+      this.completedTodos = [...this.completedTodos, tempTodo];
+    }
+  
+    // searchList function :
+    searchList(search: string) {
+          this.filteredIndexes = [];
+  
+      if (search === '') {
+        console.log('search empty');
+      } else {
+        this.todoList.forEach((todo, i) => {
+          !todo.task.toLowerCase().includes(search.toLowerCase()) &&
+            this.filteredIndexes.push(i);
+        });
+        // console.log(this.filteredIndexes);
       }
+
+
+    }
+
+
+}
